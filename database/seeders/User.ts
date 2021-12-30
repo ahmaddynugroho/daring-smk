b6a6363ca90f9d3f56099af4753fed0c529b7343
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2021-12-30 08:07:51 +0700

    feat: add seeder

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -4,11 +4,25 @@ import User from 'App/Models/User'
 export default class UserSeeder extends BaseSeeder {
   public async run() {
     // Write your database queries inside the run method
-    await User.create({
-      full_name: 'admin',
-      username: 'admin',
-      password: 'admin',
-      user_type: 'admin',
-    })
+    await User.createMany([
+      {
+        full_name: 'admin',
+        username: 'admin',
+        password: 'admin',
+        user_type: 'admin',
+      },
+      {
+        full_name: 'Joko Widodadi',
+        username: 'joko',
+        password: 'awokawok',
+        user_type: 'guru',
+      },
+      {
+        full_name: 'Muhammad Zaki',
+        username: 'zaki',
+        password: 'awokawok',
+        user_type: 'siswa',
+      },
+    ])
   }
 }
